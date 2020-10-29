3c2b718386cc539aac58b19a74bbd5852a787575
Dibyendu Mukhopadhyay <44434618+dibyenducoder@users.noreply.github.com>
2020-10-29 13:10:05 +0530

    Add files via upload

---
+++ 6th sem/Compiler Design/Assignment 3/input.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void)
+{
+	/*this is a code for our sinput file*/
+	//this is a single line comment
+	int a,b,c,d;
+	a=1000;
+	b=5;
+	c=4;
+	d=a*b+c/100;
+	printf("%d",&d);
+	return 0;
+}
